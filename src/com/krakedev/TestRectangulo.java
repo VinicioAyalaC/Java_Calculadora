ae1164a5bd7cab0955d3dddd651a238abad70fe1
VinicioAyalaC <vhac2311@gmail.com>
2026-07-18 17:58:46 -0500

    Probar metodo perimetro en el main

--- src/com/krakedev/TestRectangulo.java
+++ src/com/krakedev/TestRectangulo.java
@@ -4,7 +4,7 @@ public class TestRectangulo {
 
 	public static void main(String[] args) {
 		
-
+		//----------------------------------------------------------
 		Rectangulo r1 = new Rectangulo();		
 		r1.base = 10;
 		r1.altura = 5;
@@ -13,13 +13,22 @@ public class TestRectangulo {
 		System.out.println("El area 1 es: " + resultadoArea1 );		
 		
 		
-		
+		//----------------------------------------------------------
 		Rectangulo r2 = new Rectangulo();
 		r2.base = 8;
 		r2.altura = 6;
 		
 		int resultadoArea2 = r2.calcularArea();
 		System.out.println("El area 2 es: " + resultadoArea2);
+		
+		
+		//----------------------------------------------------------
+		Rectangulo perimetro1 = new Rectangulo();
+		perimetro1.base = 18;
+		perimetro1.altura = 12;
+		
+		double resultadoPerimetro1 = perimetro1.calcularPerimetro();
+		System.out.println("El perimetro 1 es: " + resultadoPerimetro1);
 		
 	}
 
