edf3d287bf3a5d15f7847b24849016e02fe26d08
VinicioAyalaC <vhac2311@gmail.com>
2026-07-18 16:53:37 -0400

    Funcion multiplicar

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -21,6 +21,12 @@ public class Calculadora {
 	}//fin restar
 	
 	
+	public double multiplicar(double valor1, double valor2) {
+		
+		double resultado = valor1*valor2;
+		
+		return resultado;
+	}//fin multiplicar
 	
 	
 	
