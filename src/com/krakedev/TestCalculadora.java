31a99bf78151c5b3b989d05522a66bdfb5b23656
VinicioAyalaC <vhac2311@gmail.com>
2026-07-18 17:10:26 -0400

    Validar que se ejecute todos los metodos

--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -9,11 +9,29 @@ public class TestCalculadora {
 		int resultadoSuma = c1.sumar(9, 5);		
 		System.out.println("La suma es: "+resultadoSuma);
 		
-		System.out.println("\n");
 		
+		System.out.println("\n");		
 		int resultadoResta = c1.restar(9, 5);
 		System.out.println("La resta es: "+resultadoResta);
+		
+		System.out.println("\n");
+		double resultadoMultiplicar = c1.multiplicar(9, 5);
+		System.out.println("La multiplicacion es: " + resultadoMultiplicar);
+		
+		System.out.println("\n");
+		double resultadoDividir = c1.dividir(9, 5);
+		System.out.println("La division es: " + resultadoDividir);
 
+		
+		System.out.println("\n");
+		double resultadoPromediar = c1.promediar(10, 8, 5);
+		System.out.println("El promedio es: " + resultadoPromediar);
+		
+		
+		System.out.println("\n");
+		c1.mostrarResultado();
+		
+		
 	}
 
 }
